c12c3f65f39e7380a939b3ac0362857902ac4839
michelle nderi <nderimichelle@gmail.com>
2023-06-04 23:47:06 +0300

    Created all user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+
+    def favorite_product
+        highest_rated_review = reviews.order(star_rating: :desc).first
+        highest_rated_review.product if highest_rated_review
+    end
+
+    def remove_reviews product
+        reviews.where(product: product).destroy_all
+    end
 end
+
